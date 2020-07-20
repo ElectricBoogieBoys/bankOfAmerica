9ee8368e34c714c8f3ad8fea262c2ee2e6b7b95a
unknown <william.henke@computingforall.org>
2020-07-20 16:27:34 -0700

    This is today's work on the create a username page. (7/20/2020)

--- form.js
+++ form.js
@@ -1,13 +1,30 @@
-// setting up our objects//
-let name = document.getElementById('username');
-let pass = document.getElementById('password')
-let form = document.getElementById('form');
+// // setting up our objects//
+// let name = document.getElementById('username');
+// let pass = document.getElementById('password')
+// let form = document.getElementById('form');
 
-form.addEventListener("submit", testWrite);
+// form.addEventListener("submit", testWrite);
 
-//test console logging the values//
-function testWrite(){
-  event.preventDefault();
-  console.log(name.value, pass.value);
+// //test console logging the values//
+// function testWrite(){
+//   event.preventDefault();
+//   console.log(name.value, pass.value);
   
-};
+// };
+
+let createUsername = document.getElementById('createUsername');
+let createPassword = document.getElementById('createPassword');
+let usernameRegex = 
+
+createUsername.addEventListener('keyup', validUsername);
+
+function validUsername() {
+  let createUsernameString = document.getElementById('createUsername').value;
+  let usernameCheck = str.includes(" ");
+  if(createUsernameString.length > usernameCheck) {
+    console.log('invalid username') ; 
+  }
+  else {
+    console.log('valid password');
+  }
+}
