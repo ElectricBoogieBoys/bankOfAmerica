68a39a3d5ab1c72fc805da27222b9216677a9132
Nolan Barham <nolander91b@gmail.com>
2020-07-20 15:36:19 -0700

    changed html to fit createPassword & check for identical passwords

--- form.js
+++ form.js
@@ -1,13 +1,17 @@
 // setting up our objects//
-let name = document.getElementById('username');
+let confirm = document.getElementById('confirm');
 let pass = document.getElementById('password')
 let form = document.getElementById('form');
-
+let error = document.getElementById('error')
 form.addEventListener("submit", testWrite);
 
 //test console logging the values//
 function testWrite(){
   event.preventDefault();
-  console.log(name.value, pass.value);
+  if((pass.value)===(confirm.value)){
+  console.log(confirm.value, pass.value);
+  }else{
+    error.innerHTML = "Your passwords don't seem to match, please try again"
+  }  
   
 };
