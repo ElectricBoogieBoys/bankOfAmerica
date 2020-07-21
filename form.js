246d67f726b2a68d72db7b4af2656a5dc445d7b7
unknown <william.henke@computingforall.org>
2020-07-21 13:23:57 -0700

    This will be the page for where the user inputs their first and last name.

--- form.js
+++ form.js
@@ -1,30 +1,23 @@
-// // setting up our objects//
-// let name = document.getElementById('username');
-// let pass = document.getElementById('password')
-// let form = document.getElementById('form');
-
-// form.addEventListener("submit", testWrite);
-
-// //test console logging the values//
-// function testWrite(){
-//   event.preventDefault();
-//   console.log(name.value, pass.value);
-  
-// };
-
 let createUsername = document.getElementById('createUsername');
 let createPassword = document.getElementById('createPassword');
-let usernameRegex = 
+let createAccount = document.getElementById('createAccount');
 
-createUsername.addEventListener('keyup', validUsername);
+createUsername.addEventListener('click', saveUserData);
+// let usernameRegex = 
 
-function validUsername() {
-  let createUsernameString = document.getElementById('createUsername').value;
-  let usernameCheck = str.includes(" ");
-  if(createUsernameString.length > usernameCheck) {
-    console.log('invalid username') ; 
-  }
-  else {
-    console.log('valid password');
-  }
+// createUsername.addEventListener('keyup', validUsername);
+
+// function validUsername() {
+//   // let createUsernameString = document.getElementById('createUsername').value;
+//   let usernameCheck = new RegExp(/\s/);
+//   if(usernameCheck === true) {
+//     console.log('invalid username') ; 
+//   }
+//   else {
+//     console.log('valid password');
+//   }
+// }
+
+function saveUserData() {
+  let UserData =   
 }
