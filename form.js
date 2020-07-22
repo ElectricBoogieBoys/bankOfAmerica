5b5f194f38904c55d9f40d56b3fbb6c43bb45b52
Nolan Barham <nolander91b@gmail.com>
2020-07-22 16:44:23 -0700

    made directions for passwords more visual

--- form.js
+++ form.js
@@ -2,22 +2,42 @@
 let confirm = document.getElementById('confirm');
 let pass = document.getElementById('password')
 let form = document.getElementById('form');
-let error = document.getElementById('error')
-form.addEventListener("submit", testWrite);
+let error = document.getElementById('error');
+let length = document.getElementById('length')
+let valid = document.getElementById('invalidCharacters')
+//setting up my regex
+let lengthCheck = /.{8,20}$/
+let validCheck = /[^@#*()+={}\/?~,.-_]/
+
+
+form.addEventListener('keyup', checkPass)
+
+function checkPass() {
+
+if(lengthCheck.test(pass.value)){
+  console.log('long enough')
+  length.classList.toggle("liCorrect");
+}
+if(validCheck.test(pass.value)){
+  console.log('long enough')
+  valid.classList.toggle("liCorrect");
+}
+if(length.class === 'liCorrect'){
+  console.log('class checked')
+  form.addEventListener("submit", testWrite);
+}
 
 //test console logging the values//
 function testWrite(){
   event.preventDefault();
-  let re = /^(?=.{8,20}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/
-  if(re.test(pass.value)){
-    console.log('passed regex text')
-    if((pass.value)===(confirm.value)){
-      console.log(confirm.value, pass.value);
-    }else{
-    error.innerHTML = "<p> Your passwords don't seem to match, please try again </p>"
-    } 
+  //let re = /^()(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/
+  //if(re.test(pass.value)){
+  console.log('passed regex text')
+  if((pass.value)===(confirm.value)){
+    console.log(confirm.value, pass.value);
   }else{
-    
+    error.innerHTML = "<p> Your passwords don't seem to match, please try again </p>"
   } 
-  
-};
+  //}else{ 
+    }
+  }
