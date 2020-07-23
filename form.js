94eeb221a079ff46f5ed416090c849700efb959b
Nolan Barham <nolander91b@gmail.com>
2020-07-23 13:55:14 -0700

    added more checks, still to finish 2 checks and final submit

--- form.js
+++ form.js
@@ -1,27 +1,55 @@
 // setting up our objects//
 let confirm = document.getElementById('confirm');
-let pass = document.getElementById('password')
+let pass = document.getElementById('password');
 let form = document.getElementById('form');
 let error = document.getElementById('error');
-let length = document.getElementById('length')
-let valid = document.getElementById('invalidCharacters')
+let length = document.getElementById('length');
+let valid = document.getElementById('invalidCharacters');
+let varied =document.getElementById('varied');
+let unique = document.getElementById('unique')
+//arrays and flags
+let passArray = [];
+let passwordFlag = false;
 //setting up my regex
 let lengthCheck = /.{8,20}$/
 let validCheck = /[^@#*()+={}\/?~,.-_]/
+let variedCheck = /(?=A-Z)(?=\d)(?=!$%^&;:''""<>)/
+let uniqueCheck = /[^\d{3,} a-z{3,} A-Z{3,}]/
 
 
 form.addEventListener('keyup', checkPass)
 
 function checkPass() {
-
+//checks password length
 if(lengthCheck.test(pass.value)){
-  console.log('long enough')
-  length.classList.toggle("liCorrect");
+  length.classList.add("liCorrect");
+}else{
+  length.classList.remove('liCorrect');
 }
+
+//checks if all characters are valid
 if(validCheck.test(pass.value)){
-  console.log('long enough')
-  valid.classList.toggle("liCorrect");
+  valid.classList.add("liCorrect");
+}else{
+  valid.classList.remove("liCorrect");
 }
+
+//checks if theres a capital, number and symbol
+if(variedCheck.test(pass.value)){
+  varied.classList.add("liCorrect");
+}else{
+  varied.classList.remove("liCorrect");
+}
+
+//checks if characters arent repeated
+if(uniqueCheck.test(pass.value)){
+  unique.classList.add("liCorrect");
+}else{
+  unique.classList.remove("liCorrect");
+}
+
+//final check for submit
+console.log('checks work')
 if(length.class === 'liCorrect'){
   console.log('class checked')
   form.addEventListener("submit", testWrite);
---
+++ test.js
@@ -0,0 +1,27 @@
+let createFirstName = document.getElementById('createFirstName');
+let createLastName = document.getElementById('createLastName');
+let nameSubmit = document.getElementById('nameSubmit');
+let nameArray = [];
+
+// createFirstName.addEventListener('keyup', );
+// createLastName.addEventListener('keyup', );
+nameSubmit.addEventListener('click', validNames);
+
+function validNames() {
+  event.preventDefault();
+
+  // let testCharacter = /^(?=.*\W).*$/
+
+  if(2 > 1) {
+    let saveNames = new Object(createFirstName.value + " " + createLastName.value)
+    nameArray.push(saveNames);
+    console.log('all user real names', nameArray[0]);
+    
+  }
+  if(1 > 2) {
+    console.log('bad first name');
+  }
+  else {
+    
+  }
+}
