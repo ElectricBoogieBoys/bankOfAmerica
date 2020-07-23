2be4d443c5a7c4d1e55850ca067a476638638f74
unknown <william.henke@computingforall.org>
2020-07-23 14:14:34 -0700

    Now the validNames function can console.log the msot recently added object to the nameArray array. Also, the function will give an error message if any unallowed character or numbers are unputted or if the area is left blank.

--- form.js
+++ form.js
@@ -10,21 +10,29 @@ nameSubmit.addEventListener('click', validNames);
 function validNames() {
   event.preventDefault();
 
-  // let testCharacter = /^(?=.*\W).*$/
-
-  if(2 > 1) {
+  let testCharacter = /^[^!@#$%^&*()_+={}\[\]|\\:;“’<,>.?๐฿0123456789]*$/
+  let emptyStringTest = /^$/
+  if(emptyStringTest.test(createFirstName.value) && emptyStringTest.test(createLastName.value)) {
+    console.log('invalid name input 1234');
+    document.getElementById('nameError').style.display = "block";
+  }
+  else if(testCharacter.test(createFirstName.value) && testCharacter.test(createLastName.value)) {
     let saveNames = new Object(createFirstName.value + " " + createLastName.value)
     nameArray.push(saveNames);
-    console.log('all user real names', nameArray[0]);
-    
+    let lastOfNameArray = nameArray.length - 1;
+    console.log('all user real names', nameArray[lastOfNameArray]);
+    document.getElementById('nameError').style.display = "none";
   }
-  if(1 > 2) {
-    console.log('bad first name');
+    else {
+      console.log('invalid name input');
+      document.getElementById('nameError').style.display = "block";
+    }
   }
-  else {
-    
-  }
-}
+
+// str.search("");
+// -exempt characters  ` - ~ ¨
+// - filtered characters  !@#$%^&*()_+=[{}|\/?,<>;:'"0123456789
+
 
     // localStorage.setItem('nameArray', nameArray);
     // JSON.stringify('nameArray', nameArray);
