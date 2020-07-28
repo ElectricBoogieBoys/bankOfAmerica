2c9b56ca851aa1c5c4f219d6a92a12cd212b36cb
unknown <william.henke@computingforall.org>
2020-07-28 15:04:55 -0700

    The user ID stuff has been moved to new files

--- username.js
+++ username.js
@@ -47,30 +47,3 @@ function validNames() {
       welcome.style.display = "none";
     }
   };
-
-
-  let createUsername = document.getElementById('createUsername');
-  let usernameSubmit = document.getElementById('usernameSubmit');
-
-  usernameSubmit.addEventListener('click', checkUsername);
-
-  // checkUsername will check if the username is greater than 8 character and will check if it is blank
-  // if everything is valid then the username will be saved
-  // if either of those  
-  function checkUsername() {
-    let emptyUsernameTest = /^$/
-
-    if() {
-
-    }
-    else if(){
-
-    }
-    else {
-
-    }
-  }
-
-  // If the previous stuff is correct, hide it and show create username part
-  // user inputs a username and if it fits the requirements and is not already taken it is saved for future use
-  // if it is not correct an error message is show (either 'invalid username' error or 'username taken' error)
