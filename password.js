3d4dffee5dbf1b8884320af8e0e17ba939c06b18
Nolan Barham <nolander91b@gmail.com>
2020-08-04 13:57:00 -0700

    styled and bug fixed combined register page

--- password.js
+++ password.js
@@ -6,11 +6,13 @@ let error = document.getElementById('error');
 let length = document.getElementById('length');
 let valid = document.getElementById('invalidCharacters');
 let varied =document.getElementById('varied');
-let unique = document.getElementById('unique')
+let unique = document.getElementById('unique');
+
 //arrays and flags
 let passArray = [];
 let passwordFlag = false;
 let confirmFlag = false;
+
 //setting up my regex
 let lengthCheck = /.{8,20}$/
 //let validCheck = /[A-Za-z0-9!$%^&;:''""<>]*/g
@@ -25,64 +27,65 @@ form.addEventListener('keyup', checkPass)
 
 function checkPass() {
 
-//checks password length
-if(lengthCheck.test(pass.value)){
-  length.classList.add("liCorrect");
-}else{
-  length.classList.remove('liCorrect');
-}
+  //checks password length
+  if(lengthCheck.test(pass.value)){
+    length.classList.add("liCorrect");
+  }else{
+    length.classList.remove('liCorrect');
+  }
 
-//checks if all characters are valid
-if(!invalidCharacters.test(pass.value) && lengthCheck.test(pass.value)){
-  valid.classList.add("liCorrect");
-}else{
-  valid.classList.remove("liCorrect");
-}
+  //checks if all characters are valid
+  if(!invalidCharacters.test(pass.value) && lengthCheck.test(pass.value)){
+    valid.classList.add("liCorrect");
+  }else{
+    valid.classList.remove("liCorrect");
+  }
 
-//checks if theres a capital, number and symbol
-if(variedCheck.test(pass.value)){
-  varied.classList.add("liCorrect");
-}else{
-  varied.classList.remove("liCorrect");
-}
+  //checks if theres a capital, number and symbol
+  if(variedCheck.test(pass.value)){
+    varied.classList.add("liCorrect");
+  }else{
+    varied.classList.remove("liCorrect");
+  }
 
-//checks if characters arent repeated
-if(!uniqueCheck.test(pass.value) && lengthCheck.test(pass.value)){
-  unique.classList.add("liCorrect");
-}else{
-  unique.classList.remove("liCorrect");
-}
+  //checks if characters arent repeated
+  if(!uniqueCheck.test(pass.value) && lengthCheck.test(pass.value)){
+    unique.classList.add("liCorrect");
+  }else{
+    unique.classList.remove("liCorrect");
+  }
 
-//final check for submit
+  //final check for submit
 
-if(!uniqueCheck.test(pass.value) && lengthCheck.test(pass.value) && variedCheck.test(pass.value) && !invalidCharacters.test(pass.value)){
-  passwordFlag = true;
-}
+  if(!uniqueCheck.test(pass.value) && lengthCheck.test(pass.value) && variedCheck.test(pass.value) && !invalidCharacters.test(pass.value)){
+    passwordFlag = true;
+  }
 
-if((pass.value)===(confirm.value)){
-  confirmFlag = true;
-}
+  if((pass.value)===(confirm.value)){
+    confirmFlag = true;
+  }
 
-if(passwordFlag === true && confirmFlag === true){
-  form.addEventListener("submit", testWrite);
+  if(passwordFlag === true && confirmFlag === true){
+    form.addEventListener("submit", testWrite);
+  }
 }
 
 //test console logging the values//
-function testWrite(){
+function testWrite(event){
   event.preventDefault();
-  //let re = /^()(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/
-  //if(re.test(pass.value)){
-  if((pass.value)===(confirm.value)){
-    //let savePass = new Object(confirm.value)
+  
+  if( pass.value === confirm.value){
+    
     let savePass = {
       password: confirm.value, 
     }
     passArray.push(savePass);
-    let lastOfPassArray = passArray.length - 1;
+
+    console.log('pushing it into array');
     console.log('all passwords', passArray);
   }else{
     error.innerHTML = "<p> Your passwords don't seem to match, please try again </p>"
   } 
-  //}else{ 
-    }
-  }
+ 
+}
+  
--- userId.js
+++ userId.js
@@ -2,14 +2,17 @@
 let createUsername = document.getElementById('createUsername');
 
 // calls in the submit button
-let usernameSubmit = document.getElementById('usernameSubmit');
+// let usernameSubmit = document.getElementById('usernameSubmit');
+let submitButton = document.getElementById('form');
 
 // creates a new array where the usernames will remporarily be stored
 let IDArray = [];
-
+let check1 = true;
+let check2 = true;
 // the checkUsername function will run when the submit button is clicked
-usernameSubmit.addEventListener('click', checkUsername);
-
+// usernameSubmit.addEventListener('click', checkUsername);
+createUsername.addEventListener('keyup', checkUsername);
+submitButton.addEventListener('submit', submitUsername);
   // checkUsername will check if the username is greater than 8 character and will check if it is blank
   // if everything is valid then the username will be saved
   // if either of those  
@@ -25,6 +28,8 @@ function checkUsername() {
         document.getElementById('usernameBlank').style.display = "block";
         document.getElementById('invalidUsername').style.display = "none";
         document.getElementById('usernameTaken').style.display = "none";
+        document.getElementById('goodUsername').style.display = "none";
+        let check1 = false;
         // console.log('Please enter a username.');
     } 
     // ckecks if the username is shorter than 8 characters
@@ -33,6 +38,8 @@ function checkUsername() {
         document.getElementById('usernameBlank').style.display = "none";
         document.getElementById('invalidUsername').style.display = "block";
         document.getElementById('usernameTaken').style.display = "none";
+        document.getElementById('goodUsername').style.display = "none";
+        let check2 = false;
         // console.log('Too short.');
     }
     // this will maybe be used later to check for a server error and than display a certain error message to the user
@@ -46,13 +53,23 @@ function checkUsername() {
         document.getElementById('usernameBlank').style.display = "none";
         document.getElementById('invalidUsername').style.display = "none";
         document.getElementById('usernameTaken').style.display = "none";
-        // let saveUserIds = new Object(createUsername.value);
+        document.getElementById('goodUsername').style.display = "block";
+        let check1 = true;
+        let check2 = true;
+    }
+}
+
+function submitUsername() {
+    event.preventDefault();
+    if(check1 === true && check2 === true) {
+        document.getElementById('usernameBlank').style.display = "none";
+        document.getElementById('invalidUsername').style.display = "none";
+        document.getElementById('usernameTaken').style.display = "none";
+        document.getElementById('goodUsername').style.display = "block";
         let saveUserIds = {
             username: createUsername.value,
         }
         IDArray.push(saveUserIds);
         console.log('These are the usernames: ',IDArray);
-        // let lastOfIDArray = IDArray.length - 1;
-        // console.log('all user IDs', IDArray[lastOfIDArray]);
-        }
+    }
 }
