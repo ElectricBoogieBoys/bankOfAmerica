15fd33c483ec2eea33d8c895b25d40bd84bc8164
unknown <william.henke@computingforall.org>
2020-08-05 12:44:06 -0700

    email input field and pre back end functionality implemented into the username files

--- username.js
+++ username.js
@@ -1,10 +1,14 @@
 // Calls in the form inputs
 let createFirstName = document.getElementById('createFirstName');
 let createLastName = document.getElementById('createLastName');
+let inputEmail = document.getElementById('inputEmail');
+
 // Calls in the submit button
 let nameSubmit = document.getElementById('nameSubmit');
-// This array is where the full names will be stored as objectsfor later use
+
 let nameArray = [];
+let emailArray = [];
+let emptyStringTest = /^$/
 
 // temporary test code to display a welcome message on the  landing page when the final log in is finished
 let welcome = document.getElementById('welcome');
@@ -17,17 +21,13 @@ function validNames() {
 
   // These are regex tester for the if statements
   let testCharacter = /^[^!@#$%^&*()_+={}\[\]|\\:;“’<,>.?๐฿0123456789]*$/
-  let emptyStringTest = /^$/
 
   // This tests to see if the user filled in all input fiels or not
-  // If they did not an error message will be displayed to the user
   if(emptyStringTest.test(createFirstName.value) && emptyStringTest.test(createLastName.value)) {
     // console.log('invalid name input 1234');
     document.getElementById('nameInputError').style.display = "block";
   }
   // This tests for any invalid characters that should not be in a name
-  // If either of the tests is true, an error message will be displayed to the user
-  // Otherwise, the forst and last names will be put together in an object and placed into the 'nameArray' array
   else if(testCharacter.test(createFirstName.value) && testCharacter.test(createLastName.value)) {
     // let saveNames = new Object(createFirstName.value + " " + createLastName.value);
 
@@ -35,8 +35,13 @@ function validNames() {
       name: `${createFirstName.value} ${createLastName.value}`,
     }
     nameArray.push(saveNames);
+    let saveEmail = {
+      email: inputEmail.value,
+    }
+    emailArray.push(saveEmail);
     // let lastOfNameArray = nameArray.length - 1;
     console.log('all user names', nameArray);
+    console.log('all user emails', emailArray);
     document.getElementById('nameInputError').style.display = "none";
     welcome.style.display = "none";
 
@@ -50,4 +55,4 @@ function validNames() {
       document.getElementById('nameInputError').style.display = "block";
       welcome.style.display = "none";
     }
-  };
+  }
